b47b6de3f5b751b48313c2bc8fde2bc617576204
Shize Ligit config --global user.name shize.li@sjsu.edu <54549527+lishize1234@users.noreply.github.com>
2020-10-20 01:51:30 -0700

    yes
    1

---
+++ build/precache-manifest.e52083388bea41a91e14a9130db6a130.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f79b6ec8cc12a21243778217ba1699",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfae72c64bba93d22699",
+    "url": "/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "01c86c44f1241e1468d8",
+    "url": "/static/css/main.6707e6b2.chunk.css"
+  },
+  {
+    "revision": "cfae72c64bba93d22699",
+    "url": "/static/js/2.3129728c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3129728c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c86c44f1241e1468d8",
+    "url": "/static/js/main.6ca9fafc.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "2b9fa4d51722c564cf67b0ca96e1c468",
+    "url": "/static/media/download.2b9fa4d5.jpg"
+  },
+  {
+    "revision": "07c6d56168cce19b0c46c57deb607a8b",
+    "url": "/static/media/satellite.07c6d561.svg"
+  },
+  {
+    "revision": "13dbd9ca840780deac6266a6d86426f8",
+    "url": "/static/media/starlink_logo.13dbd9ca.svg"
+  },
+  {
+    "revision": "08dbde37d6036849f0dff82d041908bb",
+    "url": "/static/media/stars_footer.08dbde37.jpg"
+  }
+]);
